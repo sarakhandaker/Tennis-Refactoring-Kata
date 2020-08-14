aaf15891a9e900334ea0500ec18237f530b26655
SaraKhandaker <kttsara@gmail.com>
2020-08-14 12:36:18 -0700

    descriptive variable names

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -181,31 +181,31 @@ end
 
 class TennisGame3
   def initialize(player1Name, player2Name)
-    @p1N = player1Name
-    @p2N = player2Name
-    @p1 = 0
-    @p2 = 0
+    @player1Name = player1Name
+    @player2Name = player2Name
+    @player1Points = 0
+    @player2Points = 0
   end
       
   def won_point(n)
-    if n == @p1N
-        @p1 += 1
+    if n == @player1Name
+        @player1Points += 1
     else
-        @p2 += 1
+        @player2Points += 1
     end
   end
   
   def score
-    if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
+    if (@player1Points < 4 and @player2Points < 4) and (@player1Points + @player2Points < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@p1]
-      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
+      s = p[@player1Points]
+      @player1Points == @player2Points ? s + "-All" : s + "-" + p[@player2Points]
     else
-      if (@p1 == @p2)
+      if (@player1Points == @player2Points)
         "Deuce"
       else
-        s = @p1 > @p2 ? @p1N : @p2N
-        (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+        s = @player1Points > @player2Points ? @player1Name : @player2Name
+        (@player1Points-@player2Points)*(@player1Points-@player2Points) == 1 ? "Advantage " + s : "Win for " + s
       end
     end
   end
