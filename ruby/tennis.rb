4fb38aae869ddc2ec0271788c3e71dc997d1ea74
SaraKhandaker <kttsara@gmail.com>
2020-08-14 14:42:48 -0700

    removed if statements

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class TennisGame1
 
   def initialize(player1Name, player2Name)
@@ -58,89 +58,49 @@ class TennisGame2
   end
       
   def won_point(playerName)
-    if playerName == @player1Name
-      p1Score()
-    else
-      p2Score()
-    end
+    playerName == @player1Name ? p1Score() : p2Score()
+  end
+
+  def p1Score
+    @p1points +=1
+  end
+  
+  def p2Score
+    @p2points +=1
   end
 
   def score
     result = ""
+    tennis_terms=["Love", "Fifteen", "Thirty", "Forty"]
     if (@p1points == @p2points and @p1points < 3)
-      if (@p1points==0)
-        result = "Love"
-      end
-      if (@p1points==1)
-        result = "Fifteen"
-      end
-      if (@p1points==2)
-        result = "Thirty"
-      end
-      result += "-All"
+        result = tennis_terms[@p1points]
+        result += "-All"
     end
     if (@p1points==@p2points and @p1points>2)
         result = "Deuce"
     end
-    
     p1res = ""
     p2res = ""
     if (@p1points > 0 and @p2points==0)
-      if (@p1points==1)
-        p1res = "Fifteen"
-      end
-      if (@p1points==2)
-        p1res = "Thirty"
-      end
-      if (@p1points==3)
-        p1res = "Forty"
-      end
+      p1res = tennis_terms[@p1points] || @p1points.to_s
       p2res = "Love"
       result = p1res + "-" + p2res
     end
     if (@p2points > 0 and @p1points==0)
-      if (@p2points==1)
-        p2res = "Fifteen"
-      end
-      if (@p2points==2)
-        p2res = "Thirty"
-      end
-      if (@p2points==3)
-        p2res = "Forty"
-      end
-      
+      p2res = tennis_terms[@p2points] || @p2points.to_s
       p1res = "Love"
       result = p1res + "-" + p2res
     end
     
     if (@p1points>@p2points and @p1points < 4)
-      if (@p1points==2)
-        p1res="Thirty"
-      end
-      if (@p1points==3)
-        p1res="Forty"
-      end
-      if (@p2points==1)
-        p2res="Fifteen"
-      end
-      if (@p2points==2)
-        p2res="Thirty"
-      end
+        p1res= tennis_terms[@p1points]
+        p2res="Fifteen" if (@p2points==1)
+        p2res="Thirty" if (@p2points==2)
       result = p1res + "-" + p2res
     end
     if (@p2points>@p1points and @p2points < 4)
-      if (@p2points==2)
-        p2res="Thirty"
-      end
-      if (@p2points==3)
-        p2res="Forty"
-      end
-      if (@p1points==1)
-        p1res="Fifteen"
-      end
-      if (@p1points==2)
-        p1res="Thirty"
-      end
+        p2res = tennis_terms[@p2points]
+        p1res= tennis_terms[@p1points]
       result = p1res + "-" + p2res
     end
     if (@p1points > @p2points and @p2points >= 3)
@@ -170,13 +130,6 @@ class TennisGame2
     end
   end
 
-  def p1Score
-    @p1points +=1
-  end
-  
-  def p2Score
-    @p2points +=1
-  end
 end
 
 class TennisGame3
